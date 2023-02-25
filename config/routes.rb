4c0bb391e338342da38de062e6e432aec0a15da6
Zach Ruiz <powersbooth10@gmail.com>
2023-02-25 12:50:48 -0600

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   # route to test your configuration
   get '/hello', to: 'application#hello_world'
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
