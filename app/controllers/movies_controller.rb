58d8cfc097da36dc50eab101b2b6c2f0ce38afda
Zach Ruiz <powersbooth10@gmail.com>
2023-02-28 11:21:45 -0600

    added index and show method to the MoviesController

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,10 @@
 class MoviesController < ApplicationController
+
+    def index
+        render json: Movie.all, status: :ok
+    end
+
+    def show
+        render json: Movie.find(params[:id]), status: :ok
+    end
 end
